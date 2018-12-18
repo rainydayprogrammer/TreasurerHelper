d21023d2faf9dcd2dbea01f788f36e7a700d2e2f
rainydayprogrammer <idic@kcn.ne.jp>
2018-12-18 21:47:04 +0900

    MaterialDesignThemesを適用

--- TreasurerHelper/ViewModels/MainWindowViewModel.cs
+++ TreasurerHelper/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ namespace TreasurerHelper.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "会計係支援システム";
         public string Title
         {
             get { return _title; }
